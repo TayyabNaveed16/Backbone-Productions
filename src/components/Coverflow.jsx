b87307ed742ea63216877982792a2fb40ae2c76e
Muhammad Tayyab Naveed Sheikh <tayyabnaveed1992@gmail.com>
2026-06-19 16:55:50 +0500

    Trying to add images

--- src/components/Coverflow.jsx
+++ src/components/Coverflow.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef } from "react";
-import GenMB_Fast from "../assets/images/projects/abc.jpg";
-import GenMB_GIKI from "../assets/images/projects/abc.jpg";
-import BinanceCommunityMeetup from "../assets/images/projects/abc.jpg";
-import BinanceCommunityMeetup_2 from "../assets/images/projects/abc.jpg";
-import JazzSalesConference from "../assets/images/projects/abc.jpg";
-import JazzSalesConference_2 from "../assets/images/projects/abc.jpg";
+import GenMB_Fast from "../assets/images/projects/GenMB_Fast.jpg";
+import GenMB_GIKI from "../assets/images/projects/GenMB_GIKI.jpg";
+import BinanceCommunityMeetup from "../assets/images/projects/BinanceCommunityMeetup.jpg";
+import BinanceCommunityMeetup_2 from "../assets/images/projects/BinanceCommunityMeetup_2.jpg";
+import JazzSalesConference from "../assets/images/projects/JazzSalesConference.jpg";
+import JazzSalesConference_2 from "../assets/images/projects/JazzSalesConference_2.jpg";
 
 
 const images = [
