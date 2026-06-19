f38d490c9860bf7aea24875179f85226622efcbb
Muhammad Tayyab Naveed Sheikh <tayyabnaveed1992@gmail.com>
2026-06-19 17:03:37 +0500

    Reverted

--- src/components/Coverflow.jsx
+++ src/components/Coverflow.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef } from "react";
-import GenMB_Fast from "../assets/images/projects/genmb_fast.jpg";
-import GenMB_GIKI from "../assets/images/projects/genmb_giki.jpg";
-import BinanceCommunityMeetup from "../assets/images/projects/binancecommunitymeetup.jpg";
-import BinanceCommunityMeetup_2 from "../assets/images/projects/binancecommunitymeetup_2.jpg";
-import JazzSalesConference from "../assets/images/projects/jazzsalesconference.jpg";
-import JazzSalesConference_2 from "../assets/images/projects/jazzsalesconference_2.jpg";
+import GenMB_Fast from "../assets/images/projects/abc.jpg";
+import GenMB_GIKI from "../assets/images/projects/abc.jpg";
+import BinanceCommunityMeetup from "../assets/images/projects/abc.jpg";
+import BinanceCommunityMeetup_2 from "../assets/images/projects/abc.jpg";
+import JazzSalesConference from "../assets/images/projects/abc.jpg";
+import JazzSalesConference_2 from "../assets/images/projects/abc.jpg";
 
 
 const images = [
